aec48d4a8accfbe022b7165b72ae21249ecc6592
Tom Petty <hello@anothertompetty.com>
2015-11-01 17:56:54 +0000

    adding hero component

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -6,4 +6,23 @@ $(document).on('ready', function() {
     $('.menu__button__icon--close').toggle();
   });
 
+  var target = $('.hero__img');
+  var targetHeight = target.height();
+  var containerHeight = $('body').outerHeight();
+
+  var maxScroll = containerHeight - targetHeight;
+  var scrollRange = maxScroll/(target.length-1);
+
+  $(document).scroll(function(e){
+
+    var scrollY = $(window).scrollTop();
+    var scrollPercent = (scrollRange - scrollY%scrollRange)/scrollRange;
+    var divIndex = Math.floor(scrollY/scrollRange);
+
+    target.has(':lt(' + divIndex + ')').css('opacity', 0);
+    target.eq(divIndex).css('opacity', scrollPercent);
+    target.has(':gt(' + divIndex + ')').css('opacity', 1);
+
+  });
+
 });
