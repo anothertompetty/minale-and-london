52f19dacae124fd35d8f1f7227e2781b82667505
Tom Petty <hello@anothertompetty.com>
2015-11-03 20:40:35 +0000

    updating general styles and content

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,5 +1,18 @@
 $(document).on('ready', function() {
 
+  // Fade in nav
+
+  $('.main').waypoint(function() {
+    
+    var header = $('.header');
+
+    header.toggle();
+    header.toggleClass('bounceInDown');
+
+  });
+
+  // Contact form modal
+
   var contactButton = $('.button--contact');
   var contactForm = $('.contact-form');
   var contactContent = $('.contact-form__content');
@@ -15,37 +28,5 @@ $(document).on('ready', function() {
   contactClose.on('click', function() {
     contactForm.fadeToggle(125)
   });
-
-  var hero = $('.hero');
-  var main = $('.main');
-  var range = hero.outerHeight();
-
-  $(window).on('scroll', function () {
-    
-      var scrollTop = main.scrollTop();
-      var offset = hero.offset().top;
-      var height = hero.outerHeight();
-      offset = offset + height;
-      var calc = 1 - (scrollTop - offset + range * 2) / range;
-    
-      hero.css({ 'opacity': calc });
-      console.log(calc);
-    
-      if (calc > '1') {
-      hero.css({'opacity': 1});
-    } else if (calc < '0') {
-      hero.css({'opacity': 0});
-    }
-    
-  });
-
-  $('.main').waypoint(function() {
-    
-    var header = $('.header');
-
-    header.toggle();
-    header.toggleClass('bounceInDown');
-
-  })
 
 });
