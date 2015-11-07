5e2eed457f33afa8e3f9cc1fa2fb8235f82ea5db
Tom Petty <hello@anothertompetty.com>
2015-11-07 15:14:27 +0000

    updating mobile styles

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -3,9 +3,7 @@ $(document).on('ready', function() {
   // Fade in nav
 
   $('.main').waypoint(function() {
-    
     var header = $('.header');
-
     header.toggle();
     header.toggleClass('bounceInDown');
 
